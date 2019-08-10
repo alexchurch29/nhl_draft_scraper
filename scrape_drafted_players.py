ee9c6397e727a80f09c97873425baac0fc0378f0
Alex Church <35304852+alexchurch29@users.noreply.github.com>
2019-08-10 12:20:19 -0400

    Add files via upload

---
+++ scrape_drafted_players.py
@@ -0,0 +1,217 @@
+import time
+import requests
+import sqlite3
+import pandas as pd
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from bs4 import BeautifulSoup
+
+
+def get_url(url):
+    """
+    Get the url
+    :param url: given url
+    :return: raw html
+    """
+
+    response = requests.Session()
+    retries = Retry(total=30, backoff_factor=.1)
+    response.mount('http://', HTTPAdapter(max_retries=retries))
+
+    try:
+        response = response.get(url, timeout=60)
+        response.raise_for_status()
+    except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError):
+        return None
+
+    return response
+
+
+def get_urls_single_year(draft_year):
+    """
+    Given a draft_year it returns the player page urls of all players drafted that year
+    :param draft_year: e.g. 2000
+    :return: list of player page urls
+    """
+
+    draft_year = str(draft_year)
+    url = 'https://www.eliteprospects.com/draft/nhl-entry-draft/{}'.format(draft_year)
+
+    try:
+        html = get_url(url)
+        time.sleep(1)
+    except Exception as e:
+        print('HTML for draft year {} is not there'.format(draft_year), e)
+        raise Exception
+
+    soup = BeautifulSoup(html.content, 'html.parser')
+    players = soup.find_all(class_="player")
+    players = [str(player.find('a')) for player in players]
+    players = [player.split('"')[1] for player in players if player.find('href="') != -1]
+
+    return players
+
+
+def get_urls_multi_year(start, stop):
+    """
+    Given a range of draft_years it returns the player page urls of all players drafted between the specified years
+    :param start: start year
+    :param stop: stop year
+    :return: list of player urls
+    """
+
+    players = list()
+
+    for i in range(start, stop + 1):
+        players += get_urls_single_year(i)
+
+    return players
+
+
+def player_profile(url):
+    """
+    Given a player page url scrapes the player profile for bio info and career stats
+    :param url: player page url
+    :return: list with player info
+    """
+
+    try:
+        html = get_url(url)
+        time.sleep(1)
+    except Exception as e:
+        print('HTML for player {} is not there'.format(url), e)
+        raise Exception
+
+    soup = BeautifulSoup(html.content, 'html.parser')
+
+    player = soup.find_all(class_='table-view')
+    player = [x.get_text().split('\n') for x in player][0]
+    player = [x.strip() for x in player if x != '']
+
+    try:
+        id = url.split('/')[4]
+        name = url.split('/')[-1].replace('-', ' ').title()
+        dob = player[player.index('Date of Birth')+1]
+        country = player[player.index('Nation')+1]
+        pos = player[player.index('Position') + 1]
+        height = player[player.index('Height') + 1][-6:]
+        weight = player[player.index('Weight') + 1][:7]
+
+        draft = player[player.index('Drafted') + 1].split(' ')
+        d_year = draft[0]
+        d_pos = draft[3][1:]
+
+        team = draft[draft.index('by')+1:]
+        d_team = ''
+        for i in team:
+            d_team += i
+            d_team += ' '
+        d_team = d_team.strip()
+
+        player_bio = list()
+        player_bio.append(id)
+        player_bio.append(name)
+        player_bio.append(dob)
+        player_bio.append(country)
+        player_bio.append(pos)
+        player_bio.append(height)
+        player_bio.append(weight)
+        player_bio.append(d_year)
+        player_bio.append(d_pos)
+        player_bio.append(d_team)
+        bio = list()
+        bio.append(player_bio)
+
+        player_bio = pd.DataFrame(bio, columns=['id', 'Name', 'DOB', 'Country', 'Pos', 'Height', 'Weight', 'Draft_Year', 'Draft_Pos', 'Draft_Team'])
+
+        player = soup.find("table", "table table-striped table-condensed table-sortable player-stats highlight-stats")
+
+        stats = list()
+        for line in player.findAll('tr'):
+            for l in line.findAll('td'):
+                stats.append(l.getText().replace('\n', '').strip())
+
+        if pos != 'G':
+            stats = [stats[i:i + 17][:9] for i in range(0, len(stats), 17) if stats[i:i + 17][5] != '-']
+
+            for i in stats:
+                if i[0] == '':
+                    i[0] = None
+                else:
+                    i[0] = i[0].replace('-', '')
+
+            stats = pd.DataFrame(stats, columns=['Season', 'Team', 'League', 'GP', 'G', 'A', 'P', 'PIM', '+/-'])
+
+        else:
+            stats = [stats[i:i + 11][:6] for i in range(0, len(stats), 11) if stats[i:i + 11][5] != '-']
+
+            for i in stats:
+                if i[0] == '':
+                    i[0] = None
+                else:
+                    i[0] = i[0].replace('-', '')
+
+            stats = pd.DataFrame(stats, columns=['Season', 'Team', 'League', 'GP', 'GAA', 'SV%'])
+
+        stats['Season'].fillna(method='ffill', inplace=True)
+        stats.insert(loc=0, column='id', value=id)
+    except:
+        return
+
+    return player_bio, stats, pos
+
+
+def parse_multi_year(start, end):
+
+    urls = get_urls_multi_year(start, end)
+
+    bios = []
+    player_stats = []
+    goalie_stats = []
+
+    for i in urls:
+        player = player_profile(i)
+        try:
+            bios.append(player[0])
+            if player[2] != 'G':
+                player_stats.append(player[1])
+            else:
+                goalie_stats.append(player[1])
+        except:
+            continue
+
+    player_bios = pd.concat(bios, ignore_index=True)
+    player_stats = pd.concat(player_stats, ignore_index=True)
+    goalie_stats = pd.concat(goalie_stats, ignore_index=True)
+
+    player_bios = pd.DataFrame.drop_duplicates(player_bios)
+    player_stats = pd.DataFrame.drop_duplicates(player_stats)
+    goalie_stats = pd.DataFrame.drop_duplicates(goalie_stats)
+
+    conn = sqlite3.connect('nhl_draft.db')
+    cur = conn.cursor()
+
+    bios.to_sql('bios', conn, if_exists='replace', index=False)
+    player_stats.to_sql('player_stats', conn, if_exists='replace', index=False)
+
+    season_stats = cur.executescript('''
+        create table skater_stats_season as
+        select t1.*, substr(t1.season,0,5) - substr(t2.dob,length(dob)+1,-4) as age, 
+        round(round(g,2)/gp,2) as G_GP, round(round(a,2)/gp,2) as A_GP, round(round(P,2)/gp,2) as P_GP
+        from player_stats t1
+        inner join bios t2 
+        on t1.id = t2.id''')
+
+    career_stats = cur.executescript('''
+            create table skater_stats_career as
+            select t1.*, round(round(t1.g,2)/t1.gp,2) as G_GP, round(round(t1.a,2)/t1.gp,2) as A_GP, round(round(t1.P,2)/t1.gp,2) as P_GP
+            from (select bios.id, league, sum(gp) as GP, sum(g) as G, sum(a) as A, sum(p) as P
+            from player_stats
+            inner join bios 
+            on player_stats.id = bios.id
+            group by player_stats.id, league) t1''')
+
+    drop = cur.executescript('''
+        DROP TABLE IF EXISTS player_stats;''')
+
+    return
---
+++ scrape_elite_prospects_api.py
@@ -0,0 +1,298 @@
+import time
+import requests
+import json
+import sqlite3
+import pandas as pd
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+
+# import eliteprospects api key from local file
+f = open('api_key.txt', 'r')
+api_key = f.read()
+
+
+def main():
+
+    conn = sqlite3.connect('nhl_draft.db')
+    cur = conn.cursor()
+
+    draft = parse_draft_json(1995, 2019)
+    draft.to_sql('draft', conn, if_exists='replace', index=False)
+
+    player_queue = draft['Player_Id'].unique().tolist() #some players get drafted more than once so we need unique
+    player_set = set()
+    player_league_queue = list()
+    goalie_league_queue = list()
+    player_league_set = set()
+    goalie_league_set = set()
+
+    player_bios = list()
+    player_stats = list()
+    goalie_stats = list()
+
+    while len(player_queue) > 0:
+        player_id = player_queue.pop()
+        if player_id not in player_set:
+            player = parse_player_json(player_id)
+            if int(player[0][0][3][:4]) > 1976:
+                player_bios.append(player[0][0])
+                if player[0][0][5] != 'G':
+                    for i in range(0, len(player[1])):
+                        player_stats.append(player[1][i])
+                else:
+                    for i in range(0, len(player[1])):
+                        goalie_stats.append(player[1][i])
+            player_set.add(player_id)
+
+    player_bios = pd.DataFrame(player_bios, columns=['Player_Id', 'First_Name', 'Last_Name', 'DOB', 'Country', 'Pos', 'Shoots', 'Height', 'Weight'])
+    player_stats = pd.DataFrame(player_stats, columns=['Player_Id', 'Season', 'Team', 'League', 'GP', 'G', 'A', 'P', 'PIM', '+/-'])
+    goalie_stats = pd.DataFrame(goalie_stats, columns=['Player_Id', 'Season', 'Team', 'League', 'GP', 'GAA', 'SVP'])
+
+    player_bios.to_sql('bios', conn, if_exists='replace', index=False)
+    player_stats.to_sql('player_stats', conn, if_exists='replace', index=False)
+    goalie_stats.to_sql('goalie_stats', conn, if_exists='replace', index=False)
+
+    drop_player_season = cur.executescript('''
+                DROP TABLE IF EXISTS skater_stats_season;''')
+    drop_player_career = cur.executescript('''
+                    DROP TABLE IF EXISTS skater_stats_career;''')
+    drop_goalie_season = cur.executescript('''
+                        DROP TABLE IF EXISTS goalie_stats_season;''')
+
+    skater_season_stats = cur.executescript('''
+            create table skater_stats_season as
+            select t1.*, substr(t1.season,0,5) - substr(t2.dob,length(dob)+1,-4) as age, 
+            round(round(g,2)/gp,2) as G_GP, round(round(a,2)/gp,2) as A_GP, round(round(P,2)/gp,2) as P_GP
+            from player_stats t1
+            inner join bios t2 
+            on t1.Player_Id = t2.Player_Id''')
+
+    skater_career_stats = cur.executescript('''
+                create table skater_stats_career as
+                select t1.*, round(round(t1.g,2)/t1.gp,2) as G_GP, round(round(t1.a,2)/t1.gp,2) as A_GP, 
+                round(round(t1.P,2)/t1.gp,2) as P_GP
+                from (select bios.Player_Id, league, sum(gp) as GP, sum(g) as G, sum(a) as A, sum(p) as P
+                from player_stats
+                inner join bios 
+                on player_stats.Player_Id = bios.Player_Id
+                group by player_stats.Player_Id, league) t1''')
+
+    goalie_season_stats = cur.executescript('''
+                create table goalie_stats_season as
+                select t1.*, substr(t1.season,0,5) - substr(t2.dob,length(dob)+1,-4) as age
+                from goalie_stats t1
+                inner join bios t2 
+                on t1.Player_Id = t2.Player_Id''')
+
+    drop_player_temp = cur.executescript('''
+            DROP TABLE IF EXISTS player_stats;''')
+    drop_goalie_temp = cur.executescript('''
+                DROP TABLE IF EXISTS goalie_stats;''')
+
+    return
+
+
+def get_url(url):
+    """
+    Get response for a given request
+    :param url: given url
+    :return: response from server
+    """
+
+    response = requests.Session()
+    retries = Retry(total=30, backoff_factor=.1)
+    response.mount('http://', HTTPAdapter(max_retries=retries))
+
+    try:
+        response = response.get(url, timeout=60)
+        response.raise_for_status()
+    except (requests.exceptions.HTTPError, requests.exceptions.ConnectionError):
+        return None
+
+    return response
+
+
+def get_draft_json(draft_year, offset):
+    """
+    Given a year it returns the eliteprospects json data for that draft
+    Ex: http://api.eliteprospects.com/beta/drafts?limit=1000&offset=0&filter=draftType.id=1%26year=2016&sort=overall:asc
+    &apikey=abcdefghijk123456
+    :param draft_year: year of the draft
+    :param offset: offset (limit for each request is 1000 records)
+    :return: eliteprospects json data for players drafted in that year
+    """
+
+    draft_year = str(draft_year)
+    offset = str(offset)
+    url = 'http://api.eliteprospects.com/beta/drafts?limit=1000&offset={}&filter=draftType.id=1%26year={}&sort=' \
+          'overall:asc&apikey={}'.format(offset, draft_year, api_key)
+
+    try:
+        response = get_url(url)
+        time.sleep(1)
+        draft_json = json.loads(response.text)
+    except requests.exceptions.HTTPError as e:
+        print('Json for draft {} not returned.'.format(draft_year), e)
+        return None
+
+    return draft_json
+
+
+def get_player_stats(player_id):
+    """
+    Given a player id it returns the eliteprospects json data for that players career stats
+    Ex: http://api.eliteprospects.com/beta/players/199898/stats?limit=1000&sort=season.name:asc&apikey=abcdefghijk123456
+    :param player_id: player id
+    :return: eliteprospects json data for that player's career stats
+    """
+
+    player_id = str(player_id)
+    url = 'http://api.eliteprospects.com/beta/players/{}/stats?limit=1000&sort=season.name:asc&apikey={}'.format(
+        player_id, api_key)
+
+    try:
+        response = get_url(url)
+        time.sleep(1)
+        player_json = json.loads(response.text)
+    except requests.exceptions.HTTPError as e:
+        print('Json for player {} not returned.'.format(player_id), e)
+        return None
+
+    return player_json
+
+
+def parse_draft_json(start_year, end_year):
+    """
+    Parses the eliteprospects json for a given range of draft years
+    :param start_year: first year to include in timeframe
+    :param end_year: final year to include in timeframe
+    :return: pandas df with player ids, draft year, round, overall draft selection, and team
+    """
+
+    players = list()
+
+    while start_year <= end_year:
+
+        offset = 0
+        draft_json = get_draft_json(start_year, offset)
+        total_count = draft_json['metadata']['totalCount']
+
+        while True:
+
+            for i in range(0, len(draft_json['data'])):
+                player = list()
+                try:
+                    player.append(draft_json['data'][i]['player']['id'])
+                    player.append(draft_json['data'][i]['year'][:4])
+                    player.append(draft_json['data'][i]['round'])
+                    player.append(draft_json['data'][i]['overall'])
+                    player.append(draft_json['data'][i]['team']['name'])
+                    players.append(player)
+                except:
+                    player.append('-1')
+                    player.append(draft_json['data'][i]['year'][:4])
+                    player.append(draft_json['data'][i]['round'])
+                    player.append(draft_json['data'][i]['overall'])
+                    player.append(draft_json['data'][i]['team']['name'])
+                    players.append(player)
+
+            offset += 1000
+            if offset > total_count:
+                break
+            draft_json = get_draft_json(start_year, offset)
+
+        start_year += 1
+
+    draft_data = pd.DataFrame(players, columns=['Player_Id', 'Year', 'Round', 'OV', 'Team'])
+
+    return draft_data
+
+
+def parse_player_json(player_id):
+    """
+    Parses the eliteprospects json for a given player's stats
+    :param player_id: id of player
+    :return: pandas df for player bios and career stats
+    """
+
+    bios = list()
+    stats = list()
+    player_json = get_player_stats(player_id)
+
+    player = list()
+    player.append(player_json['data'][0]['player']['id'])
+    try:
+        player.append(player_json['data'][0]['player']['firstName'])
+    except:
+        player.append(None)
+    try:
+        player.append(player_json['data'][0]['player']['lastName'])
+    except:
+        player.append(None)
+    try:
+        player.append(player_json['data'][0]['player']['dateOfBirth'])
+    except:
+        player.append(None)
+    try:
+        player.append(player_json['data'][0]['player']['country']['name'])
+    except:
+        player.append(None)
+    try:
+        player.append(player_json['data'][0]['player']['playerPositionDetailed'])
+    except:
+        player.append(None)
+    try:
+        player.append(player_json['data'][0]['player']['shoots'])
+    except:
+        player.append(None)
+    try:
+        player.append(player_json['data'][0]['player']['height'])
+    except:
+        player.append(None)
+    try:
+        player.append(player_json['data'][0]['player']['weight'])
+    except:
+        player.append(None)
+    bios.append(player)
+
+    for i in range(0, len(player_json['data'])):
+        if player_json['data'][i]["gameType"] == "REGULAR_SEASON":
+            season = list()
+            if player_json['data'][0]['player']['playerPositionDetailed'] != 'G':
+                try:
+                    season.append(player_json['data'][i]['player']['id'])
+                    season.append(player_json['data'][i]['season']['name'])
+                    season.append(player_json['data'][i]['team']['name'])
+                    season.append(player_json['data'][i]['league']['name'])
+                    season.append(player_json['data'][i]['GP'])
+                    season.append(player_json['data'][i]['G'])
+                    season.append(player_json['data'][i]['A'])
+                    season.append(player_json['data'][i]['TP'])
+                    season.append(player_json['data'][i]['PIM'])
+                    try:
+                        season.append(player_json['data'][i]['PM'])
+                    except:
+                        season.append(None)
+                    stats.append(season)
+                except:
+                    pass
+            else:
+                try:
+                    season.append(player_json['data'][i]['player']['id'])
+                    season.append(player_json['data'][i]['season']['name'])
+                    season.append(player_json['data'][i]['team']['name'])
+                    season.append(player_json['data'][i]['league']['name'])
+                    season.append(player_json['data'][i]['GP'])
+                    season.append(player_json['data'][i]['GAA'])
+                    season.append(player_json['data'][i]['SVP'])
+                    stats.append(season)
+                except:
+                    pass
+
+    return bios, stats
+
+
+if __name__ == '__main__':
+    main()
+
+f.close()
